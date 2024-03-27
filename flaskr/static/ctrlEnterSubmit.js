317ced2afcfb166c76d41d544da28deb6e8baf89
Christophe Thibaut <cthibauttof@gmail.com>
2024-03-27 10:40:47 +0100

    meta key in update form

---
+++ flaskr/static/ctrlEnterSubmit.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var textarea = document.getElementById("body");
+    var submitButton = document.getElementById("save");
+
+    textarea.addEventListener("keydown", function(event) {
+        if (event.metaKey && event.keyCode === 13) { // Check if Ctrl+Enter is pressed
+            event.preventDefault(); // Prevent default newline behavior
+            submitButton.click(); // Trigger click event of the submit button
+        }
+    });
+});
+
